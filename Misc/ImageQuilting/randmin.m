1ccd261fca6a6e5a75594a6296c0bc56c416f3f4
chuanli <cl.chuanli@gmail.com>
2015-08-17 21:14:05 +0200

    delete all files

--- Misc/ImageQuilting/randmin.m
+++
@@ -1,8 +0,0 @@
-% Given a vector X, find all minima and return a random one
-%
-function [val, ind] = randmin(x);
-  
-  perm = randperm(length(x));
-  [val ind] = min(x(perm));
-  ind = perm(ind);
-  
